5157eaac405f3e1b1e2a521294ea5f00e33660d8
abdi <abdi_2020@hotmail.co.uk>
2020-03-15 16:02:28 +0000

    first commit

---
+++ calc.py
@@ -0,0 +1,15 @@
+num1 = int(input("please enter number one: "))
+oper = input("please enter operator: ")
+num2 = int(input("please enter the second number: "))
+
+if oper == "+":
+  print(num1 + num2)
+
+elif oper == "-":
+  print(num1 - num2)
+
+elif oper == "*":
+  print(num1 * num2)
+
+elif oper == "/":
+  print(num1 / num2)
